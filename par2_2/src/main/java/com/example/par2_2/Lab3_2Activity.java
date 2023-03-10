8929eaaf31ba47e3719ac8021747019410d0480e
Park <wasf4294@naver.com>
2023-03-10 23:02:02 +0900

    xml vs Java

---
+++ par2_2/src/main/java/com/example/par2_2/Lab3_2Activity.java
@@ -0,0 +1,14 @@
+package com.example.par2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab3_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab32);
+    }
+}
---
+++ par2_2/src/main/java/com/example/par2_2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.par2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout linear = new LinearLayout(this);
+
+        Button bt = new Button(this);
+        bt.setText("Button 1");
+        linear.addView(bt);
+
+        Button bt2 = new Button(this);
+        bt2.setText("Button 2");
+        linear.addView(bt2);
+
+        setContentView(linear);
+    }
+}
